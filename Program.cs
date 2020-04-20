8c18607c9176df3710d72e73765221ac3457bdcf
unknown <lvova.nikol@mail.ru>
2020-04-20 17:40:35 +0300

    Начало работы

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWork14
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
