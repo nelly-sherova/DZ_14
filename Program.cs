48d0995731ba89b7b706bcd0ea3bd993c58ad352
unknown <lvova.nikol@mail.ru>
2020-04-20 17:50:08 +0300

    Объявление методов

--- Program.cs
+++ Program.cs
@@ -2,7 +2,25 @@
 
 namespace HomeWork14
 {
-    
+    static class Calculator
+    {
+        public static T Sum<T>(T a, T b)
+        {
+            return a+b;
+        }
+        public static T Difference<T>(T a, T b)
+        {
+            return a-b;
+        }
+        public static T Multiplication<T>(T a, T b)
+        {
+            return a*b;
+        } 
+        return static T Division<T>(T a, T b)
+        {
+            return a/b;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
